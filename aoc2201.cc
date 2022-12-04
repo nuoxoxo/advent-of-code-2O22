973ffa7878caf11e855ce09b568433ffd6b20e63
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-04 08:56:55 +0100

    update day 1 cpp

--- aoc2201.cc
+++ aoc2201.cc
@@ -1,6 +1,5 @@
 #include "iostream"
 #include "vector"
-
 #include "algorithm"
 
 using	namespace std;
@@ -9,25 +8,15 @@ int	main()
 {
 	vector<int>	cl;
 	string		s;
-	int		n = 0;
+	int		r = 0;
 
 	while (getline(cin, s))
 	{
 		if (s == "")
-		{
-			cl.push_back(n);
-			n = 0;
-		}
-		else
-		{
-			n += stoi(s);
-		}
+			cl.push_back(r);
+		r = s == "" ? 0 : r + stoi(s);
 	}
-
-	n = *max_element(cl.begin(), cl.end());
-	cout << "Star 1: " << n << endl;
-
 	sort(cl.begin(), cl.end(), greater<int>());
-	n = cl[0] + cl[1] + cl[2];
-	cout << "Star 2: " << n << endl;
+	cout << "Star 1: " << cl[0] << endl;
+	cout << "Star 2: " << cl[0] + cl[1] + cl[2] << endl;
 }
