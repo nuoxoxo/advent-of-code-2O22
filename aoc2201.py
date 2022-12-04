7e04058549a9fc2d532c8868331b48ed551aac07
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-04 08:57:56 +0100

    update day 1 py

--- aoc2201.py
+++ aoc2201.py
@@ -1,26 +1,16 @@
 cl = []
 n = 0
-
 # with open('test') as file:
 with open('2201.0') as file:
     for line in file:
         if line != '':
             line = line[:len(line) - 1]
-        # print(line)
         if line == '':
             cl.append(n)
             n = 0
         else:
             n += int(line)
-
 cl.sort()
+
 print("Star 1:", cl[-1])
 print("Star 2:", sum(cl[-3:]))
-
-# before cleanup
-
-"""
-print("Star 1:", max(cl))
-cl.sort(reverse=True)
-print("Star 2:", cl[0] + cl[1] + cl[2])
-"""
