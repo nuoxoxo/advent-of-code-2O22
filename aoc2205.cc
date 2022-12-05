cd9531f22de7eeb5b3188e765c16b5393d35398e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-05 08:59:59 +0100

    unite 2 parts

--- aoc2205.cc
+++ aoc2205.cc
@@ -1,17 +1,125 @@
-#include "h.hpp"
+#include "iostream"
+#include "vector"
+#include "sstream"
+#include "deque"
+
+using	namespace std;
 
 int	main()
 {
-	vector<string>	a;
-	string		s;
-	int		res = 0, res2 = 0;
+	vector<string>		a;
+	string			s;
+	int			i;
+	char			c;
+	string			res1, res2;
+	// int			res, res2;
+	vector<deque<char>>	vd1, vd2;
 
-	while (cin >> s !cin.eof())
+	// 1st half of input - crates
+
+	while (getline(cin, s))
 	{
-		cout << s << endl;
+		if (s == "")
+			break ;
 		a.push_back(s);
 	}
 
-	cout << "Star 1: " << res << endl;
-	cout << "Star 1: " << res2 << endl;
+	// total num of crates
+
+	stringstream	ss(a[a.size() - 1]);
+	int		numc;
+
+	while (!ss.eof() && ss >> numc)
+		;;
+
+	// parse those crates
+
+	i = -1;
+	while (++i < numc)
+	{
+		vd1.push_back(deque<char>());
+		vd2.push_back(deque<char>());
+	}
+	a.pop_back();
+	for (string line : a)
+	{
+		// 1st crate
+		i = 0;
+		if (line[1] <= 'Z' && line[1] >= 'A')	
+		{
+			vd1[i].push_front(line[1]);
+			vd2[i].push_front(line[1]);
+		}
+		// subsequent crates
+		while (++i < numc)
+		{
+			c = line[1 + 4 * i];
+			if (!(c <= 'Z' && c >= 'A'))
+				continue ;
+			vd1[i].push_front(c);
+			vd2[i].push_front(c);
+		}
+	}
+	
+	// 2nd half of input - operations
+	
+	while (getline(cin, s))
+	{
+		int	Move, From, To;
+		int	move, from, to;
+
+		sscanf(s.c_str(), "move %d from %d to %d", &Move, &From, &To);
+		cout << Move << ' ' << From << ' ' << To << endl;
+
+		// part 1
+
+		from = From - 1;
+		to = To - 1;
+		move = Move;
+		i = -1;
+		while (++i < move)
+		{
+			if (vd1[from].empty())
+				continue ;
+
+			c = vd1[from].back();
+
+			vd1[from].pop_back();
+			vd1[to].push_back(c);
+		}
+
+		// part 2
+
+		string		E;
+
+		from = From - 1;
+		to = To - 1;
+		move = Move;
+		i = -1;
+		while (++i < move)
+		{
+			if (vd2[from].empty())
+				continue ;
+
+			char 	n = vd2[from].back();
+
+			E += n;
+			vd2[from].pop_back();
+		}
+		while (E != "")
+		{
+			c = E[E.length() - 1];
+			E.pop_back();
+			vd2[to].push_back(c);
+		}
+	}
+	i = -1;
+	while (++i < numc)
+	{
+		res1 += vd1[i].back();
+		res2 += vd2[i].back();
+	}
+
+	cout << "Star 1: " << res1 << endl;
+	cout << "Star 2: " << res2 << endl;
 }
