bf1252711f5905f0b5335661bed08ad3849ef0db
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-06 06:25:04 +0100

    draft day 6

--- aoc2206.cc
+++ aoc2206.cc
@@ -4,11 +4,62 @@ int	main()
 {
 	vector<string>	a;
 	string		s;
+	int		n, i, j;
+	int		r1, r2;
+
+	int		p = 4;
+	int		p2 = 14;
 
 	while (cin >> s)
 	{
 		a.push_back(s);
-		cout << s << endl;
-
+	}
+	for (string& l : a)
+	{
+		bool ok = false;
+		
+		n = (int) l.length();
+		i = -1;
+		while (++i < n - 4)
+		{
+			s = l.substr(i, 4);	
+			sort(s.begin(), s.end());
+			j = 0;
+			ok = false;
+			while (++j < 4 && !ok)
+			{
+				if (s[j - 1] == s[j])
+				{
+					ok = true;
+					break ;
+				}
+			}
+			if (!ok)
+			{
+				r1 = i + 4;
+				break;
+			}
+		}
+		i = -1;
+		ok = false;
+		while (++i < n - 4)
+		{
+			s = l.substr(i, 4);	
+			sort(s.begin(), s.end());
+			j = 0;
+			while (++j < 4 && !ok)
+			{
+				if (s[j - 1] == s[j])
+				{
+					ok = true;
+					break ;
+				}
+			}
+			if (!ok)
+			{
+				r1 = i + 4;
+				break;
+			}
+		}
 	}
 }
