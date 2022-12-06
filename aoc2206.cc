3f2f6996111156c00efa0c33eae794acac049dbb
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-06 06:33:07 +0100

    cleaning up . still has loop

--- aoc2206.cc
+++ aoc2206.cc
@@ -2,21 +2,17 @@
 
 int	main()
 {
-	vector<string>	a;
-	string		s;
-	int		n, i, j;
-	int		r1, r2;
+	string	s;
+	int	n, i, j;
+	int	r1, r2;
+	int	p2 = 14;
+	string	l
 
-	int		p = 4;
-	int		p2 = 14;
 
-	while (cin >> s)
-	{
-		a.push_back(s);
-	}
+	cin >> s;
 	for (string& l : a)
 	{
-		bool ok = false;
+		bool	ok = false;
 		
 		n = (int) l.length();
 		i = -1;
@@ -41,13 +37,13 @@ int	main()
 			}
 		}
 		i = -1;
-		ok = false;
-		while (++i < n - 4)
+		while (++i < n - p2)
 		{
-			s = l.substr(i, 4);	
+			s = l.substr(i, p2);	
 			sort(s.begin(), s.end());
 			j = 0;
-			while (++j < 4 && !ok)
+			ok = false;
+			while (++j < p2 && !ok)
 			{
 				if (s[j - 1] == s[j])
 				{
@@ -57,9 +53,11 @@ int	main()
 			}
 			if (!ok)
 			{
-				r1 = i + 4;
+				r2 = i + p2;
 				break;
 			}
 		}
 	}
+	cout << "Star 1: " << r1 << endl;
+	cout << "Star 2: " << r2 << endl;
 }
