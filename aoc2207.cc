a3f7ac70d174405bed78b84691c174577aec5968
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-07 08:14:02 +0100

    clean up . fi. day 7

--- aoc2207.cc
+++ aoc2207.cc
@@ -4,39 +4,27 @@ bool	is_number(string &);
 
 int	main()
 {
-	map<string, int> mp;
 	map<string, int>::iterator it;
+	map<string, int> mp;
 	vector<string>	a;
-	vector<string>	lv; // current dir
-	string		s;
-	int		i, np;
+	vector<string>	lv; // current lvl
+	string		s, sls = "/";
+	int		i, np; // res;
+	int		ii;
+	long long	r;
+
 	long long	ll, togo;
 	long long 	top=4*(int)1e7;
 
 	while (getline(cin, s))
 		a.push_back(s);
-	long long	r = 0;
-	for (string & l: a)
+	r = 0;
+	for (string& l: a)
 	{
 		stringstream	ss(l);
 		string		n;
 
 		ss >> n;
-		/*
-		if (is_number(n))
-		{
-			cout << "number - " << n << endl;
-			long long t = stoll(n);
-			cout << "number parsed - " << n << endl;
-			if (t < 100000)
-			{
-				cout << "current - " << r << '+'<< endl;
-				r = r + t;
-				cout << "current - " << t << endl;
-				cout << "current - " << r << endl;
-			}
-		}
-		*/
 		if (n == "$")
 		{
 			ss >> n;
@@ -44,76 +32,57 @@ int	main()
 				continue ;
 			ss >> n;
 			if (n == "..")
-			{
 				lv.pop_back();
-			}
 			else
-			{
 				lv.push_back(n);
-			}
 		}
-		if (is_number(n))
+		else if (is_number(n))
 		{
-			cout << "number - " << n << endl;
 			ll = stoll(n);
-			//np = (int) lv;
-			//cout << "number parsed - " << n << endl;
-			
-			mp["/"] += ll;
 			i = 0;
-			cout << lv.size() << endl;
+			mp[sls] += ll;
+			// cout << lv.size() << " levels" << endl;
 			while (++i < (int) lv.size())
 			{
-				string	tmp = "/";
-				int	ii = -1;
+				string	tmp = sls;
+				
+				ii = -1;
 				while (++ii < i + 1)
-				{
-					tmp += lv[ii] + "/";
-				}
-				cout << tmp << endl;
+					tmp += lv[ii] + sls;
+				// cout << "composed: " << tmp << endl;
 				mp[tmp] += ll;
 			}
 		}
-		/*
-		if (n == "$")
-		{
-			ss >> n;
-			if (n == "cd")
-				cout << "cd - " << l << endl;
-			if (n == "ls")
-				cout << "ls - " << l << endl;
-		}
-		else
-		{
-			cout << "else - " << l << endl;
-		}
-		*/
 	}
-	long long r2 = (int) 1e9;
-	ll = mp["/"];
+
+	long long	r2 = (int) 1e9;
+
+	ll = mp[sls];
 	togo = ll - top;
 	it = mp.begin();
 	while (it != mp.end())
 	{
-		cout << it->first << endl;
+		// cout << it->first << endl;
+		
 		if (it->second < 100000)
 			r += it->second;
 		if (it->second > togo)
-			r2 = it->second < r2? it->second: r2;
+			r2 = it->second < r2 ? it->second : r2;
 		it++;
 	}
+	// cout << "\n";
 
 	
-	cout << r << endl;
-	cout << r2 << endl;
+	cout << "Star 1: " << r << endl;
+	cout << "Star 2: " << r2 << endl;
 }
 
-bool	is_number(string & s)
+bool	is_number(string &s)
 {
 	string::iterator	it;
 
 	it = s.begin();
-	while (it != s.end() && isdigit(*it))
+	while (it != s.end() && isdigit(* it))
 		++it;
-	return !s.empty() && it == s.end();
+	return ( !s.empty() && it == s.end());
 }
