9f86395018bfc12e092c4071ebffb4e158b4c3d1
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-10 08:46:28 +0100

    fi. day 10 cc

--- aoc2210.cc
+++ aoc2210.cc
@@ -1,14 +1,75 @@
-#include "h.hpp"
+#include "iostream"
+#include "vector"
+#include "sstream"
+
+using	namespace std;
 
 int	main()
 {
-	vector<string>	a;
+	vector<int>	a;
 	string		s;
+	int		r1, n, x, cc, i, ii;
+	int		len, row;
+	string		r2;	
 
-	while (cin >> s)
+	while (getline(cin, s))
 	{
-		a.push_back(s);
-		cout << s << endl;
+		stringstream	ss(s);
+		
+		a.push_back(0);
+		if (s == "noop")
+			continue ;
+		ss >> s >> n;
+		a.push_back(n);
 	}
-	cout << "(" << a.size() << " lines)\n";
+
+	// part 1
+
+	r1 = 0;
+	x = 1; // X
+	i = 0;
+	len = (int) a.size();
+	cc = 0; // cycle
+	while (1)
+	{
+		if (i == len)
+			i %= len;
+		n = a[i];
+		ii = i + 1;
+		if (ii == 20 || ii % 40 == 20)
+			r1 += ii * x;
+		x += n;
+		++i;
+		++cc;
+		if (cc == 220)
+			break ;
+	}
+
+	// part 2
+
+	x = 1;
+	i = 0;
+	cc = 0;
+	r2 = "\n";
+	while (1)
+	{
+		if (i == len)
+			i %= len;
+		row = i % 40;
+		// if (!((i + 1) % 40))
+		// 	r2 += '\n';
+		if (row == x - 1 || row == x || row == x + 1)
+			r2 += '@';
+		else
+			r2 += ' ';
+		if (!((i + 1) % 40))
+			r2 += '\n';
+		x += a[i];
+		++i;
+		++cc;
+		if (cc == 240) // (?)
+			break ;
+	}
+	cout << "Star 1: " << r1 << endl;
+	cout << "Star 2: " << r2 << endl;
 }
