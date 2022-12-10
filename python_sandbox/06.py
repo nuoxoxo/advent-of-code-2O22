a1d2bebf242b34d42b0d161a3cafb9559ea2d185
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-10 19:28:45 +0100

    ed. path day 6 py

--- python_sandbox/06.py
+++ python_sandbox/06.py
@@ -23,13 +23,13 @@ def solve(file) -> Tuple[int, int]:
     yield r1
     yield r2
 
-with open('2206.0') as file:
+with open('../_inputs/2206.0') as file:
     r1, r2 = solve(file)
     print('data')
     print('Star 1:', r1)
     print('Star 2:', r2, end='\n\n')
 
-with open('2206.1') as file:
+with open('../_inputs/2206.1') as file:
     print('test')
     r1, r2 = solve(file)
     print('Star 1:', r1)
