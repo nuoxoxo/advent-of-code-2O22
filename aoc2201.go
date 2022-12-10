c996c3f869ddf9a22e45761052248ceeafd79918
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-10 19:25:04 +0100

    + day 1 in Go

--- aoc2201.go
+++ aoc2201.go
@@ -8,10 +8,21 @@ import (
 	"sort"
 )
 
-var path string = "_inputs/2201.0"
-// var path string = "_inputs/2201.1"
-
 func main() {
+	var r1, r2 int
+
+	r1, r2 = solve("_inputs/2201.0")
+	fmt.Println("data")
+	fmt.Println("Star 1:", r1)
+	fmt.Println("Star 2:", r2, "\n")
+
+	r1, r2 = solve("_inputs/2201.1")
+	fmt.Println("test")
+	fmt.Println("Star 1:", r1)
+	fmt.Println("Star 2:", r2)
+}
+
+func solve(path string) (int, int) {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		panic(err)
@@ -52,6 +63,5 @@ func main() {
 	sort.Sort(sort.Reverse(sort.IntSlice(a)))
 	res2 := a[0] + a[1] + a[2]
 
-	fmt.Println("Star 1:", res)
-	fmt.Println("Star 2:", res2)
+	return res, res2
 }
