ed17de1c9421d6d1b2f99021653ec34d620c40f0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-10 18:12:16 +0100

    + day 2 in Go

---
+++ aoc2202.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	_ "io/ioutil"
+	_ "strconv"
+)
+
+var path string = "_inputs/2202.0"
+// var path string = "_inputs/2202.1"
+
+func main() {
+	data, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	var a [][]string
+	scanner := bufio.NewScanner(data)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		temp := strings.Split(scanner.Text(), " ")
+		a = append(a, temp)
+	}
+
+	// solve
+
+	res := 0
+	rr := 0
+	res2 := 0
+	d := map[string]int {
+		"A": 1, "B": 2, "C": 3,
+		"X": 1, "Y": 2, "Z": 3,
+	}
+	for _, line := range a {
+		l, r := d[line[0]], d[line[1]]
+
+		// part 1
+
+		if l == r {
+			res += r + 3
+		} else {
+			res += r
+			rr = l + 1
+			if rr > 3 {
+				rr = 1
+			}
+			if rr == r {
+				res += 6
+			}
+		}
+
+		// part 2
+
+		if r == 2 {
+			res2 += l + 3
+		} else if r == 1 {
+			l -= 1
+			if l < 1 {
+				l = 3
+			}
+			res2 += l
+		} else {
+			l += 1
+			if l > 3 {
+				l = 1
+			}
+			res2 += l + 6
+		}
+	}
+	fmt.Println("Star 1:", res)
+	fmt.Println("Star 2:", res2)
+}
