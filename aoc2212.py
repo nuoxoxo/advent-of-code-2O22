3d82959ac4c74abb668e5e71ca924254ef7a0c1f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-11 12:02:16 +0100

    prep . day 12 . py

---
+++ aoc2212.py
@@ -0,0 +1,15 @@
+a = []
+
+# with open('2212.1') as file:
+# with open('2212.0') as file:
+with open('test') as file:
+    for line in file:
+        l = line.strip()# .split()
+        print(l)
+        a.append(l)
+
+
+print(a)
+
+
+
