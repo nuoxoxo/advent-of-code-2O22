ee040d405ed2663fd647f4b0627db0e709d895f6
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-11 08:56:02 +0100

    fi. day 10

--- different_solutions/aoc2211_initial_submit.py
+++ different_solutions/aoc2211_initial_submit.py
@@ -1,10 +1,8 @@
-import copy
-
+rang = 10000
 M, m = [], []
-M2 = []
-
-# with open('_inputs/2211.1') as file:
-with open('_inputs/2211.0') as file:
+for n in M: print(n)
+# with open('test') as file:
+with open('2211.0') as file:
     for line in file:
         if 'Start' in line: # 0
             line = line.strip()[16:]
@@ -28,20 +26,19 @@ with open('_inputs/2211.0') as file:
         if line == '\n':
             m.append(0) # 5 . times
             M.append(m)
-            M2.append(copy.deepcopy(m))
             m = []
 m.append(0)
 M.append(m)
-M2.append(copy.deepcopy(m))
-
-"""
 for n in M: print(n)
-print("(parsed)\n")
-"""
 
-# part 1
+common = 1
+for n in M:
+    print(common, n[2])
+    common *= n[2]
 
-for _ in range(20):
+for _ in range(rang):
+    # if _ % 999 == 0:
+    #     for n in M: print(n)
     for m in M:
         while len(m[0]) != 0:
             n = m[0].pop(0)
@@ -51,44 +48,23 @@ for _ in range(20):
             else:
                 R = m[1][1]
             if '*' in m[1]:
-                n = n * R // 3
+                n = n * R
+                # n = n * R // 3
             elif '+' in m[1]:
-                n = (n + R) // 3
-            if n % m[2] == 0:
-                to = m[3]
-            else:
-                to = m[4]
-            M[to][0].append(n)
-r = sorted([m[-1] for m in M], reverse=True)
-res = r[0] * r[1]
-
-# part 2
-
-common = 1
-for n in M2:
-    common *= n[2]
-
-for _ in range(10000):
-    for m in M2:
-        while len(m[0]) != 0:
-            n = m[0].pop(0)
-            m[5] += 1
-            if m[1][1] == 'old':
-                R = n
-            else:
-                R = m[1][1]
-            if '*' in m[1]:
-                n *= R
-            elif '+' in m[1]:
-                n += R
+                n = (n + R)
+                # n = (n + R) // 3
             n %= common
             if n % m[2] == 0:
+            # if n % common == 0:
                 to = m[3]
+                M[to][0].append(n)
             else:
                 to = m[4]
-            M2[to][0].append(n)
-r = sorted([ m[-1] for m in M2 ], reverse=True)
-res2 = r[0] * r[1]
-
-print('Star 1:', res)
-print('Star 2:', res2)
+                M[to][0].append(n)
+print('1st round:')
+for n in M: print(n)
+r = []
+for n in M:
+    r.append(n[-1])
+r.sort(reverse=True)
+print(r[0] * r[1])
