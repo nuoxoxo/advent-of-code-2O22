9a2be907728dda1684ec855fcd933294fcbf71cb
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-17 08:26:39 +0100

    day 16 . 1st dropped day . parsing

--- aoc2216.py
+++ aoc2216.py
@@ -1,9 +1,19 @@
 from collections import deque, defaultdict
 import sys
 
-path = '2216.0'
-#path = '2216.1'
+# path = '2216.0'
+path = '2216.1'
 file = open(path).read().strip()
 a = [x for x in file.split('\n')]
-
-print(a)
+M = dict()
+for s in a:
+    ss = s.split(';')
+    vv, ra = ss[0].split()[1], int(ss[0].split('=')[1])
+    to = ss[1].split('to ')[1].split()
+    to.pop(0)
+    for i in range(len(to)):
+        if ',' in to[i]:
+            to[i] = to[i][:len(to[i])-1]
+    print('(parsing):', vv, ra, to)
+    M[vv] = (ra, to)
+for k, v in M.items(): print(k, v)
