878d3f90d590824a1108a7d8dc9f7c3ade526eae
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-18 19:29:29 +0100

    added day 18 in pybox

--- python_sandbox/18.py
+++ python_sandbox/18.py
@@ -1,39 +1,89 @@
-path = '2218.0'
-# path = '2218.1'
-prefix = '_inputs/'
+def main():
+    pre = '../_inputs/'
+    path = '2218.0'
+    # path = '2218.1'
+    file = open(pre + path).read().strip()
+    res, res2 = solve(file)
+    print('Star 1:', res)
+    print('Star 2:', res2)
 
-file = open(prefix + path).read().strip()
-a = [x for x in file.split('\n')]
-print(a)
+def solve(file) -> (int, int):
+    a = [x for x in file.split('\n')]
+    all_cubes = []
+    outmost = -1 # p2
+    for l in a:
+        cube = tuple(int(_) for _ in l.split(','))
+        all_cubes.append(cube)
+        for c in cube:
+            if outmost < c:
+                outmost = c # p2
+    print('\noutmost corner:', outmost, end='\n\n')
 
-aa = []
-for l in a:
-    print(l)
-    aa.append(tuple(int(_) for _ in l.split(',')))
-# print(aa)
+    D = [
+        (1, 0, 0),
+        (0, 1, 0),
+        (0, 0, 1),
+        (-1,0, 0),
+        (0,-1, 0),
+        (0, 0,-1),
+        ]
 
-D = [
-    (1, 0, 0),
-    (0, 1, 0),
-    (0, 0, 1),
-    (-1,0, 0),
-    (0,-1, 0),
-    (0, 0,-1),
-    ]
+    cube_face = []
+    for cube in all_cubes:
+        for face in D:
+            L = []
+            for i in range(3): L.append(cube[i] + face[i])
+            if tuple(int(_) for _ in L) not in all_cubes:
+                cube_face.append((cube, face))
+    yield len(cube_face)
 
-res = 0
-for C in aa:
-    for d in D:
-        L = []
-        for i in range(3): L.append(C[i] + d[i])
-        if tuple(int(_) for _ in L) not in aa:
-            res += 1
-print(res)
+    # part 2
+    ## (not floodfill, just brute force)
 
-"""
-# (p2) ... exactly one cube of air is trapped within the lava droplet
+    outside_faces = []
+    outside_faces_size = set()
 
-out = set()
-for C in aa:
-    out.add(C)
-"""
+    outside_cubes = set()
+    count = 0
+    size = 0
+
+    while True:
+        count += 1
+        size = len(outside_faces)
+        print(f'{size}\toutside faces (iteration {count})')
+        if size != 0 and size in outside_faces_size:
+            res2 = size
+            print(f'{res2}\t{outside_faces_size}', end='\n\n')
+            break
+        else:
+            outside_faces_size.add(size)
+        for cf in cube_face:
+            if cf in outside_faces:
+                continue
+            cube, face = cf
+            seen = set()
+            for dist in range(1, outmost):
+                x = cube[0] + face[0] * dist
+                y = cube[1] + face[1] * dist
+                z = cube[2] + face[2] * dist
+                new_cube = (x, y, z)
+                seen.add(new_cube)
+                if new_cube in all_cubes:
+                    break
+                if new_cube in outside_cubes:
+                    outside_cubes |= seen
+                    outside_faces.append(cf)
+                    break
+                ok = False
+                for c in range(3):
+                    if new_cube[c] < 0 or new_cube[c] > 21:
+                        outside_cubes |= seen
+                        outside_faces.append(cf)
+                        ok = True
+                        break
+                if ok:
+                    break
+    yield size
+
+if __name__ == '__main__':
+    main()
