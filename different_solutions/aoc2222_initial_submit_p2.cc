312eb5060163f77d17e46aac35e53f74929b411e
nuoxoxo <nuxu@e1r2p16.clusters.42paris.fr>
2022-12-23 13:54:23 +0100

    fi. day 22 . cc

--- different_solutions/aoc2222_initial_submit_p2.cc
+++ different_solutions/aoc2222_initial_submit_p2.cc
@@ -1,15 +1,14 @@
 #include "iostream"
+
 #include "sstream"
 #include "vector"
-
 using	namespace std;
 
 string	split_movement(string&);
-bool	str_is_number(const string&);
-int	a_mod_b(int a, int b) { return (b + (a % b)) % b; }
+bool str_is_number(const string&);
 
-int	solve(vector<string>& G, string& movement);
-int	solve2(vector<string>& G, string& movement);
+int	solve(vector<string> G, string& movement);
+int	solve2(vector<string> G, string& movement);
 
 int	main()
 {
@@ -24,6 +23,10 @@ int	main()
 		G.push_back(s);
 		W = max((int) s.length(), W);
 	}
+	// cout << "(original grid) \n";
+	// for (string &x: G) cout << x << endl;
+
+	// fill lines with 0s
 	for (string &x: G)
 	{
 		i = -1;
@@ -31,18 +34,94 @@ int	main()
 		while (++i < W - len)
 			x += ' ';
 	}
+	// cout << "(trailing 0s filled)\n";
+	// for (string &x: G) cout << x << endl;
+	
 	cin >> s;
 	string movement = split_movement(s);
+	// cout << "(movement parsed) \n";
+	// cout << movement << " / len: " << W << endl;
 	int res, res2;
 	res = solve(G, movement);
 	cout << "\nStar 1: " << res << endl;
+	
 	res2 = solve2(G, movement);
 	cout << "\nStar 2: " << res2 << endl;
+	// Find starting point
+	/*
+	int r = 0;
+	int c = 0;
+	while (G[0][c] != '.') ++c;
+	cout << "(starting point) " << r << ' ' << c << endl;
+	C = (int) G[0].size();
+	R = (int) G.size();
+	int dr = 0;
+	int dc = 1; // current facing: East
+	int rr, cc;
+	stringstream ss(movement);
+	while (!ss.eof() && ss >> s)
+	{
+		if (str_is_number(s))
+		{
+
+			int val = stoi(s);
+			i = -1;
+			while (++i < val)
+			{
+				rr = r;
+				cc = c;
+				while (1)
+				{
+					//rr = (dr + rr) % R;
+					//cc = (dc + cc) % C;
+					// again, buggy C++ modulo 
+					// do this: (b + (a % b)) % b
+					rr = (R + (dr + rr) % R) % R;
+					cc = (C + (dc + cc) % C) % C;
+					if (G[rr][cc] != ' ')
+						break ;
+				}
+				if (G[rr][cc] == '#')
+					break ;
+				r = rr;
+				c = cc;
+			}
+		}
+		if (s == "L")
+		{
+			swap(dr, dc);
+			dr *= -1;
+		}
+		if (s == "R")
+		{
+			swap(dr, dc);
+			dc *= -1;
+		}
+	}
+	cout << "(final facing) " << dr << ' ' << dc << endl;
+	int x = -1;
+	if (dr)
+	{
+		if (dr == 1)
+			x = 1;
+		else
+			x = 3;
+	}
+	else
+	{
+		if (dc == 1)
+			x = 0;
+		else
+			x = 2;
+	}
+	int res = 1000 * (r + 1) + 4 * (c + 1) + x;
+	cout << "\nStar 1: " << res << ' ' << R << ' ' << C << endl;
+	*/
 }
 
 //	part 2
 
-int	solve2(vector<string>& G, string& movement)
+int	solve2(vector<string> G, string& movement)
 {
 	int r = 0;
 	int c = 0;
@@ -202,22 +281,30 @@ int	solve2(vector<string>& G, string& movement)
 	}
 	cout << "(final facing) " << dr << ' ' << dc << endl;
 	int x = -1;
-	if (dr && dr == 1)
-		x = 1;
-	if (dr && dr ^ 1)
-		x = 3;
-	if (!dr && dc == 1)
-		x = 1;
-	if (!dr && dc ^ 1)
-		x = 2;
+	if (dr)
+	{
+		if (dr == 1)
+			x = 1;
+		else
+			x = 3;
+	}
+	else
+	{
+		if (dc == 1)
+			x = 0;
+		else
+			x = 2;
+	}
 	int res = 1000 * (r + 1) + 4 * (c + 1) + x;
-	cout << "(res) " << res << " at " << R << ',' << C << endl;
+	cout << res << " at " << R << ',' << C << endl;
 	return res;
 }
 
+
+
 //	part 1
 
-int	solve(vector<string>& G, string& movement)
+int	solve(vector<string> G, string& movement)
 {
 	int r = 0;
 	int c = 0;
@@ -243,8 +330,12 @@ int	solve(vector<string>& G, string& movement)
 				cc = c;
 				while (1)
 				{
-					rr = a_mod_b(dr + rr, R);
-					cc = a_mod_b(dc + cc, C);
+					//rr = (dr + rr) % R;
+					//cc = (dc + cc) % C;
+					// again, buggy C++ modulo 
+					// do this: (b + (a % b)) % b
+					rr = (R + (dr + rr) % R) % R;
+					cc = (C + (dc + cc) % C) % C;
 					if (G[rr][cc] != ' ')
 						break ;
 				}
@@ -267,16 +358,22 @@ int	solve(vector<string>& G, string& movement)
 	}
 	cout << "(final facing) " << dr << ' ' << dc << endl;
 	int x = -1;
-	if (dr && dr == 1)
-		x = 1;
-	if (dr && dr ^ 1)
-		x = 3;
-	if (!dr && dc == 1)
-		x = 1;
-	if (!dr && dc ^ 1)
-		x = 2;
+	if (dr)
+	{
+		if (dr == 1)
+			x = 1;
+		else
+			x = 3;
+	}
+	else
+	{
+		if (dc == 1)
+			x = 0;
+		else
+			x = 2;
+	}
 	int res = 1000 * (r + 1) + 4 * (c + 1) + x;
-	cout << "(res) " << res << " at " << R << ',' << C << endl;
+	cout << res << " at " << R << ',' << C << endl;
 	return res;
 }
 
